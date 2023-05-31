0a38cc05b1ed35212983a8117274ee1b61305716
TheLittleBoy <wangxa@edu-edu.com.cn>
2023-05-31 15:50:45 +0800

    Initial Commit

---
+++ GumboDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GumboDemo
+//
+//  Created by Mac on 2023/5/31.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
